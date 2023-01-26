8f06897110298ac16ccc02708cf43eebed9b5647
yasingunay <chalouper@icloud.com>
2023-01-26 16:45:41 +0100

    up

--- philo/main.c
+++ philo/main.c
@@ -6,52 +6,66 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:53:57 by ygunay            #+#    #+#             */
-/*   Updated: 2023/01/19 16:25:28 by ygunay           ###   ########.fr       */
+/*   Updated: 2023/01/26 16:37:53 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void* dine(void* arg) {
-    t_philo* philosopher = (t_philo*) arg;
-    printf("Philosopher %d is thinking\n", philosopher->id);
-    while (1) {
-        printf("Philosopher %d is hungry\n", philosopher->id);
-        pthread_mutex_lock(&philosopher->left_fork);
-        pthread_mutex_lock(&philosopher->right_fork);
-        printf("Philosopher %d is eating\n", philosopher->id);
-        sleep(1);
-        pthread_mutex_unlock(&philosopher->left_fork);
-        pthread_mutex_unlock(&philosopher->right_fork);
-        sleep(1);
-    }
-    return NULL;
+
+pthread_mutex_t mutex;
+
+
+
+void* routine()
+{
+	int i = 0;
+	while(i < 1)
+	{
+		pthread_mutex_lock(&mutex);
+		printf("Philo says hello\n");
+		pthread_mutex_unlock(&mutex);
+		
+		i++;
+	}
+	return 	NULL;
 }
 
-int main() {
-    t_philo philosophers[NUM_PHILOSOPHERS];
-    int i = 0;
-    while (i < NUM_PHILOSOPHERS) {
-        philosophers[i].id = i;
-        pthread_mutex_init(&philosophers[i].left_fork, NULL);
-        pthread_mutex_init(&philosophers[i].right_fork, NULL);
-        i++;
-    }
-    i = 0;
-    while (i < NUM_PHILOSOPHERS) {
-        pthread_create(&philosophers[i].thread, NULL, dine, &philosophers[i]);
-        i++;
-    }
-    i = 0;
-    while (i < NUM_PHILOSOPHERS) {
-        pthread_join(philosophers[i].thread, NULL);
-        i++;
-    }
-    i = 0;
-    while (i < NUM_PHILOSOPHERS) {
-        pthread_mutex_destroy(&philosophers[i].left_fork);
-        pthread_mutex_destroy(&philosophers[i].right_fork);
-        i++;
-    }
-    return 0;
+
+
+int main(int ac, char **av)
+{
+	t_setup setup;
+
+	setup.rules= malloc(sizeof(t_rules));
+	if(!setup.rules)
+		ft_error("Memory allocayion is failed");
+
+	if(ac == 2)
+		setup.rules->nb_philo= ft_atoi(av[1]);
+	else	
+	{
+		printf("argument number is not correct\n");
+		return 1;	
+	}
+		
+	pthread_t philo[setup.rules->nb_philo];
+	int i = 0;
+	pthread_mutex_init(&mutex,NULL);
+	while(i < setup.rules->nb_philo)
+	{
+		pthread_create(&philo[i], NULL, &routine, NULL);
+		i++;
+	}
+	i = 0;
+	while(i < setup.rules->nb_philo)
+	{
+		pthread_join(philo[i], NULL);
+		i++;
+	}
+	pthread_mutex_destroy(&mutex);
+	
+	return (0);
 }
+
+
--- philo/utils.c
+++ philo/utils.c
@@ -1,42 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/19 14:36:10 by ygunay            #+#    #+#             */
-/*   Updated: 2023/01/26 16:38:04 by ygunay           ###   ########.fr       */
+/*   Created: 2023/01/23 12:37:36 by ygunay            #+#    #+#             */
+/*   Updated: 2023/01/26 16:37:02 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "philo.h"
 
-typedef struct s_rules
+int	ft_error(char *str)
 {
-	int nb_philo;
-} t_rules;
+	printf("Error:\n%s\n", str);
+	return (-1);
+}
 
-typedef struct s_philo
+int ft_atoi(const char *str)
 {
-	int id;
-	t_rules *rules;
-} t_philo;
+	int nbr;
+	int i;
 
-
-typedef struct s_setup
-{
-	t_rules			*rules;
-	t_philo			*philos;
-	pthread_t		*threads;
-	pthread_mutex_t	*mutexes;
-}					t_setup;
-
-
-
-
-int ft_atoi(const char *str);
-int	ft_error(char *str);
+	nbr = 0;
+	i = 0;
+	
+	while(str[i])
+	{
+		if(str[i] < 48 || str[i] > 57)
+			return (-1);
+		i++;
+	}
+	i = 0;
+	while(str[i])
+	{
+		nbr = (nbr * 10) +str[i] - 48;
+		i++;
+	}
+	return(nbr);
+}
