8b166981ac16713a5ae3fb25eb986b3b0b876fc9
yasingunay <chalouper@icloud.com>
2023-01-27 12:08:08 +0100

    up

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:53:57 by ygunay            #+#    #+#             */
-/*   Updated: 2023/01/26 16:37:53 by ygunay           ###   ########.fr       */
+/*   Updated: 2023/01/27 12:06:28 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ pthread_mutex_t mutex;
 
 void* routine()
 {
+	t_data *data;
+
 	int i = 0;
 	while(i < 1)
 	{
 		pthread_mutex_lock(&mutex);
-		printf("Philo says hello\n");
+		printf("Philo %d says hello\n", data->philos[i].id);
 		pthread_mutex_unlock(&mutex);
 		
 		i++;
@@ -31,38 +33,49 @@ void* routine()
 	return 	NULL;
 }
 
+void create_philos(t_data *data)
+{
+	int i = 0;
+	data->philos = malloc(data->nb_philo *sizeof(t_philo));
+
+	while(i < data->nb_philo)
+	{
+		
+		pthread_create(&data->philos[i].thread, NULL, &routine, NULL);
+		data->philos[i].id = i+1;
+		//printf("%d\n",data->philos[i].id);
+		i++;
+	}
+	
+}
 
 
 int main(int ac, char **av)
 {
-	t_setup setup;
-
-	setup.rules= malloc(sizeof(t_rules));
-	if(!setup.rules)
-		ft_error("Memory allocayion is failed");
+	t_data data;
 
 	if(ac == 2)
-		setup.rules->nb_philo= ft_atoi(av[1]);
-	else	
+		data.nb_philo = ft_atoi(av[1]);
+		else	
 	{
 		printf("argument number is not correct\n");
 		return 1;	
 	}
-		
-	pthread_t philo[setup.rules->nb_philo];
-	int i = 0;
+	
+	create_philos(&data);
+	
 	pthread_mutex_init(&mutex,NULL);
-	while(i < setup.rules->nb_philo)
+
+
+	int i =0;
+	while(i < data.nb_philo)
 	{
-		pthread_create(&philo[i], NULL, &routine, NULL);
+		
+		pthread_join(data.philos[i].thread, NULL);
 		i++;
 	}
-	i = 0;
-	while(i < setup.rules->nb_philo)
-	{
-		pthread_join(philo[i], NULL);
-		i++;
-	}
+	
+	
 	pthread_mutex_destroy(&mutex);
 	
 	return (0);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:36:10 by ygunay            #+#    #+#             */
-/*   Updated: 2023/01/26 16:38:04 by ygunay           ###   ########.fr       */
+/*   Updated: 2023/01/27 11:40:34 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-typedef struct s_rules
-{
-	int nb_philo;
-} t_rules;
+
 
 typedef struct s_philo
 {
 	int id;
-	t_rules *rules;
+	pthread_t thread;
 } t_philo;
 
-
-typedef struct s_setup
+typedef struct s_data
 {
-	t_rules			*rules;
-	t_philo			*philos;
-	pthread_t		*threads;
-	pthread_mutex_t	*mutexes;
-}					t_setup;
+	int nb_philo;
+	t_philo *philos;
+} t_data;
 
 
 
