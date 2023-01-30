e63e41e9d09bbdee8e67670a838edda3f03b95be
yasingunay <chalouper@icloud.com>
2023-01-30 11:13:12 +0100

    up

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:53:57 by ygunay            #+#    #+#             */
-/*   Updated: 2023/01/27 17:53:17 by ygunay           ###   ########.fr       */
+/*   Updated: 2023/01/30 11:11:52 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,33 +23,75 @@ void* routine(void *ptr)
 	int i = 0;
 	while(i < 1)
 	{
-		pthread_mutex_lock(&mutex);
-		//printf("Philo says hello\n");
-		printf("Philo %d says hello\n", philo->id);
-		sleep(3);
-		pthread_mutex_unlock(&mutex);
+		pthread_mutex_lock(philo->left_fork);
+		pthread_mutex_lock(philo->right_fork);
+		printf("Philo %d is eating\n", philo->id);
+		sleep(1);
+		pthread_mutex_unlock(philo->left_fork);
+		pthread_mutex_unlock(philo->right_fork);
 		
 		i++;
 	}
 	return 	NULL;
 }
 
-void create_philos(t_data *data)
+
+int create_forks(t_data *data)
+{
+	int i;
+
+	i = 0;
+	data->forks = malloc(data->nb_philo * sizeof(t_mutex));
+	if(!data->forks)
+		return(-1);
+
+	while(i < data->nb_philo)
+	{
+		if(pthread_mutex_init(&data->forks[i],NULL))
+			return(-1);
+		
+		i++;
+	}
+	return (1);
+	
+}
+
+
+int create_table(t_data *data)
 {
 	int i = 0;
 	data->philos = malloc(data->nb_philo *sizeof(t_philo));
+	if(!data->philos)
+		ft_error("philo malloc error");
+
+	if(!create_forks(data))
+		return(-1);	
 
 	while(i < data->nb_philo)
 	{
 		data->philos[i].id = i+1;
+		data->philos[i].left_fork = &data->forks[i];
+		data->philos[i].right_fork = &data->forks[(i + data->nb_philo -1) % data->nb_philo];
+		
 		pthread_create(&data->philos[i].thread, NULL, &routine, &data->philos[i]);
 		
-		//printf("%d\n",data->philos[i].id);
 		i++;
 	}
-	
+	return (1);
 }
 
+int	free_threads(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		pthread_mutex_destroy(&data->forks[i]);
+		i++;
+	}
+	return (0);
+}
 
 int main(int ac, char **av)
 {
@@ -57,18 +99,18 @@ int main(int ac, char **av)
 
 	if(ac == 2)
 		data.nb_philo = ft_atoi(av[1]);
-		else	
+	else	
 	{
 		printf("argument number is not correct\n");
 		return 1;	
 	}
 	
-	pthread_mutex_init(&mutex,NULL);
-	
-	create_philos(&data);
-	
+
+	create_table(&data);
 	
 
+	
+	
 
 	int i =0;
 	
@@ -80,7 +122,8 @@ int main(int ac, char **av)
 	}
 	
 	
-	pthread_mutex_destroy(&mutex);
+	free_threads(&data);
+	
 	
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yasingunay <yasingunay@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:36:10 by ygunay            #+#    #+#             */
-/*   Updated: 2023/01/27 12:33:51 by yasingunay       ###   ########.fr       */
+/*   Updated: 2023/01/30 10:54:38 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,23 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
+typedef pthread_mutex_t	t_mutex;
 
 typedef struct s_philo
 {
 	int id;
 	pthread_t thread;
+	t_mutex	*left_fork;
+	t_mutex	*right_fork;
+	
+	
 } t_philo;
 
 typedef struct s_data
 {
 	int nb_philo;
 	t_philo *philos;
+	t_mutex *forks;
 } t_data;
 
 
